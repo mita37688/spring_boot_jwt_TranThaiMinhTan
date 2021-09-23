f34283acc4502fc5f31f23fea9d3b2fffbe95871
MSI\Minh Tan <tanlun04112000@gmail.com>
2021-09-23 09:02:09 +0700

    Started

---
+++ src/main/java/com/se/spring_boot_jwt_tranthaiminhtan/SpringBootJwtTranThaiMinhTanApplication.java
@@ -0,0 +1,13 @@
+package com.se.spring_boot_jwt_tranthaiminhtan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtTranThaiMinhTanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtTranThaiMinhTanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/se/spring_boot_jwt_tranthaiminhtan/SpringBootJwtTranThaiMinhTanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.spring_boot_jwt_tranthaiminhtan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtTranThaiMinhTanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
